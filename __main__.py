c08c7f64fc1c6bee46bdbf902d8bc8f8e03326c0
Jeremy Crafts <gravitypriest@users.noreply.github.com>
2016-03-09 18:56:53 -0500

    generalize precheck

--- __main__.py
+++ __main__.py
@@ -36,8 +36,7 @@ def load_config_file():
          'source_dir': SOURCE_DIR,
          'pgcdemux': PGCDEMUX,
          'vsrip': VSRIP,
-         'delaycut': DELAYCUT,
-         'dgindex': DGINDEX})
+         'delaycut': DELAYCUT})
     try:
         config.read(CONF_FILE)
     except ConfigParser.Error:
@@ -172,11 +171,11 @@ def pre_check(args, config):
     Make sure directories are correct
     and required programs are installed
     '''
-    def dgindex_check():
-        dgindex = config.get(APP_NAME, 'dgindex')
-        logger.debug('DGIndex path: %s' % dgindex)
-        if not os.path.isfile(dgindex):
-            logger.error('Path to DGIndex \"%s\" is invalid.' % dgindex)
+    def exe_check(name):
+        exe = config.get(APP_NAME, name.lower())
+        logger.debug('%s path: %s' % (name, exe))
+        if not os.path.isfile(exe):
+            logger.error('Path to %s \"%s\" is invalid.' % (name, exe))
             return True
         return False
 
@@ -184,23 +183,15 @@ def pre_check(args, config):
     bad_conf = False
     if args.command is 'demux':
         if not (args.no_vid and args.no_aud):
-            pgcdemux = config.get(APP_NAME, 'pgcdemux')
-            logger.debug('PGCDemux path: %s' % pgcdemux)
-            if not os.path.isfile(pgcdemux):
-                logger.error('Path to PGCDemux \"%s\" is invalid.' % pgcdemux)
-                bad_conf = True
+            bad_conf = exe_check('PGCDemux')
         if not args.no_sub:
-            vsrip = config.get(APP_NAME, 'vsrip')
-            logger.debug('VSRip path: %s' % vsrip)
-            if not os.path.isfile(vsrip):
-                logger.error('Path to VSRip \"%s\" is invalid.' % vsrip)
-                bad_conf = True
+            bad_conf = exe_check('VSRip')
         if args.avs:
-            bad_conf = dgindex_check()
+            bad_conf = exe_check('DGIndex')
     if args.command is 'avisynth':
-        bad_conf = dgindex_check()
+        bad_conf = exe_check('DGIndex')
     if args.command is 'audio':
-        pass
+        bad_conf = exe_check('DelayCut')
     if bad_conf:
         sys.exit(1)
     else:
