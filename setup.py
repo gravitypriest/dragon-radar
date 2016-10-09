ad2190baa3a58d926268a86c71e2364fc65a00da
Jeremy Crafts <gravitypriest@users.noreply.github.com>
2016-10-09 16:56:41 -0400

    update setup file

--- setup.py
+++ setup.py
@@ -1,15 +1,19 @@
-from distutils.core import setup
 import py2exe
+from distutils.core import setup
+from constants import VERSION
 
 setup(zipfile=None,
+      version=VERSION,
       console=[
-          {'script': '__init__.py',
+          {'script': '__main__.py',
            'dest_base': 'dragon-radar',
            'icon_resources': [(0, 'icon.ico')]
            }],
       data_files=[('params', ['params/episodes.json',
                               'params/demux.json',
                               'params/offsets.json',
+                              'params/titles.json',
+                              'params/title-times.json',
                               'params/valid.json']),
                   ('ac3files', ['ac3files/blank_20_192.ac3',
                                 'ac3files/blank_20_384.ac3',
